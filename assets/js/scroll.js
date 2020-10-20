8033b2eb4bcc5246903f4a18e9abad71f51de96a
gawgaw <gawgaw.developer@gmail.com>
2020-10-20 22:19:13 +0900

    パララックス効果を追加

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,65 +1,65 @@
 $(function() {
+  // 各要素がInViewしたら各要素にanimateCSSを適用
+
+  // ロゴ
   $('.logo').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('.logo').addClass('animated rollIn');
     } else {
-    //表示領域から出た時
       $('.logo').removeClass('animated rollIn');
     }
   });
+
+  // 各セクション
   $('.js-fade-in-up').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('.js-fade-in-up').addClass('animated fadeInUp');
     } else {
-    //表示領域から出た時
       $('.js-fade-in-up').removeClass('animated fadeInUp');
     }
   });
+
+  // 商品ページの中間イメージ
   $('.js-pulse').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('.js-pulse').addClass('animated pulse');
     } else {
-    //表示領域から出た時
       $('.js-pulse').removeClass('animated pulse');
     }
   });
+
+  // トップページの最初の画像その1
   $('#intro-box01').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('#intro-box01').addClass('animated fadeIn');
     } else {
-    //表示領域から出た時
       $('#intro-box01').removeClass('animated fadeIn');
     }
   });
+
+  // トップページの最初の画像その2
   $('#intro-box02').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('#intro-box02').addClass('animated fadeIn');
     } else {
-    //表示領域から出た時
       $('#intro-box02').removeClass('animated fadeIn');
     }
   });
   
+  // メンバープロフィールのテキストその1
   $('#profile-card__text01').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('#profile-card__text01').addClass('animated fadeInLeft');
     } else {
-    //表示領域から出た時
       $('#profile-card__text01').removeClass('animated fadeInLeft');
     }
   });
+
+  // メンバープロフィールのテキストその2
   $('#profile-card__text02').on('inview', function(event, isInView) {
     if (isInView) {
-    //表示領域に入った時
       $('#profile-card__text02').addClass('animated fadeInLeft');
     } else {
-    //表示領域から出た時
       $('#profile-card__text02').removeClass('animated fadeInLeft');
     }
   });
